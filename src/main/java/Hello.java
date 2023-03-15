396057c24a8e0257497fa6379179b78a0cc421e1
kimkanghyun <tjm12345max@gmail.com>
2023-03-15 09:41:09 +0900

    소스 수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello!! My First Java Coding!!");
         System.out.println("만나서 반갑습니다");
+        System.out.println("my name is kimkanghyun");
     }
 }
