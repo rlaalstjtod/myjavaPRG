526ed8e867b974b4d69b77f11e3cf719d899b841
kimkanghyun <tjm12345max@gmail.com>
2023-03-29 11:45:25 +0900

    소스 수정

---
+++ src/main/java/Ex06_15.java
@@ -0,0 +1,15 @@
+public class Ex06_15 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 10; i++) {
+
+            System.out.println(i + "단 시작합니다.");
+
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+            System.out.println( i + "단 끝났습니다.\n");
+            }
+        }
+    }
+
